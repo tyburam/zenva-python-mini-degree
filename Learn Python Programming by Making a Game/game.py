2b51e5699f332c16b4ae0761507a9dba3dbada0a
mateusztybura <tyburam@hotmail.com>
2017-10-28 21:44:14 +0200

    More cleaning, alpha channel, white background

--- Learn Python Programming by Making a Game/game.py
+++ Learn Python Programming by Making a Game/game.py
@@ -1,17 +1,22 @@
 import pygame
 
-pygame.init()
+def load_img(path, width, height):
+    scaled  = pygame.transform.scale(pygame.image.load(path),(width,height))
+    return scaled.convert_alpha()
 
+pygame.init()
 screen = pygame.display.set_mode((900,700))
-finished = False
-x = 0
-y = 50
 frame = pygame.time.Clock()
 
-playerImage = pygame.image.load("images/player.png")
-playerImage = pygame.transform.scale(playerImage, (30, 30))
-playerImage= playerImage.convert()
+blue = (0,0,255)
+black = (0,0,0)
+white = (255, 255, 255)
 
+playerX = 0
+playerY = 50
+playerImage = load_img("images/player.png", 30, 30)
+
+finished = False
 while not finished:
     for event in pygame.event.get():
         if pygame.QUIT == event.type:
@@ -21,15 +26,9 @@ while not finished:
     if 1 == pressedKeys[pygame.K_SPACE]:
         y += 5
 
-    color = (0,0,255)
-    black = (0,0,0)
-    white = (255, 255, 255)
+    screen.fill(white)
 
-    screen.fill(black)
-
-    rectOne = pygame.Rect(x,y,30,30)
-    screen.blit(playerImage, (x,y))
-    #pygame.draw.rect(screen, color, rectOne)
+    screen.blit(playerImage, (playerX,playerY))
 
     pygame.display.flip()
     frame.tick(30)
