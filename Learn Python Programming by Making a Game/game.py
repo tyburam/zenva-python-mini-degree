0a0f5af6bdc7930249141a0484d0c29e5300e366
mateusztybura <tyburam@hotmail.com>
2017-10-28 21:10:33 +0200

    Almost moving

--- Learn Python Programming by Making a Game/game.py
+++ Learn Python Programming by Making a Game/game.py
@@ -4,13 +4,16 @@ pygame.init()
 
 screen = pygame.display.set_mode((900,700))
 finished = False
+x = 0
+y = 50
 
 while not finished:
     for event in pygame.event.get():
         if pygame.QUIT == event.type:
             finished = True
-
-    rectOne = pygame.Rect(0,0,30,30)
+    
+    pressedKeys = pygame.key.get_pressed()
+    rectOne = pygame.Rect(x,y,30,30)
     color = (0,0,255)
     pygame.draw.rect(screen, color, rectOne)
     pygame.display.flip()
