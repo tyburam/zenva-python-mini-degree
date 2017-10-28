d90d321f0dac69c082069e7be5fcaf75e33e9850
mateusztybura <tyburam@hotmail.com>
2017-10-28 21:00:08 +0200

    Blue rectangle

--- Learn Python Programming by Making a Game/game.py
+++ Learn Python Programming by Making a Game/game.py
@@ -9,3 +9,8 @@ while not finished:
     for event in pygame.event.get():
         if pygame.QUIT == event.type:
             finished = True
+
+    rectOne = pygame.Rect(0,0,30,30)
+    color = (0,0,255)
+    pygame.draw.rect(screen, color, rectOne)
+    pygame.display.flip()
