a1960bd1bb4936b5dcc6b8f08915e3ff66f104cb
mateusztybura <tyburam@hotmail.com>
2017-10-28 21:20:55 +0200

    Moving

--- Learn Python Programming by Making a Game/game.py
+++ Learn Python Programming by Making a Game/game.py
@@ -13,7 +13,14 @@ while not finished:
             finished = True
     
     pressedKeys = pygame.key.get_pressed()
-    rectOne = pygame.Rect(x,y,30,30)
+    if 1 == pressedKeys[pygame.K_SPACE]:
+        y += 5
+
     color = (0,0,255)
+    black = (0,0,0)
+
+    screen.fill(black)
+
+    rectOne = pygame.Rect(x,y,30,30)
     pygame.draw.rect(screen, color, rectOne)
     pygame.display.flip()
