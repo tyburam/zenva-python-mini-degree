5295f2959e937fe225dd0ae353c731c4f755b5d6
mateusztybura <tyburam@hotmail.com>
2017-10-29 20:23:48 +0100

    Many enemies. Names. Collisions

--- Learn Python Programming by Making a Game/game.py
+++ Learn Python Programming by Making a Game/game.py
@@ -14,16 +14,21 @@ screen = pygame.display.set_mode((900,700))
 frame = pygame.time.Clock()
 
 blue = (0,0,255)
+red = (255, 0, 0)
 black = (0,0,0)
 white = (255, 255, 255)
 
 playerX = 450 - (35/2)
 playerY = 650
 enemyX = playerX
-enemyY = 450
+enemyY = 570
+enemyMovingRight = True
 treasureX = playerX
 treasureY = 50
 level = 1
+
+enemies = [(enemyX, enemyY, enemyMovingRight)]
+enemyNames = {0:"Max", 1:"Jill", 2:"Greg", 3:"Diane"}
 
 playerImage = load_img("images/player.png", 35, 40).convert_alpha()
 backgroundImage = load_img("images/background.png", 900, 700)
@@ -32,22 +37,24 @@ enemyImage = load_img("images/enemy.png", 35, 40).convert_alpha()
 font = pygame.font.SysFont("comicsans", 70)
 
 finished = False
-enemyMovingRight = True
-
 while not finished:
     for event in pygame.event.get():
         if pygame.QUIT == event.type:
             finished = True
-    
-    if enemyX >= 820:
-        enemyMovingRight = False
-    elif enemyX <= 30:
-        enemyMovingRight = True
-        
-    if enemyMovingRight:
-        enemyX += 5
-    else:
-        enemyX -= 5
+
+    enemyIndex = 0
+    for enemyX, enemyY, enemyMovingRight in enemies:
+        if enemyX >= 800:
+            enemyMovingRight = False
+        elif enemyX <= 40:
+            enemyMovingRight = True
+
+        if enemyMovingRight:
+            enemyX += 5 * level
+        else:
+            enemyX -= 5 * level
+        enemies[enemyIndex] = (enemyX, enemyY, enemyMovingRight)
+        enemyIndex += 1
 
     pressedKeys = pygame.key.get_pressed()
     if 0 != pressedKeys[pygame.K_SPACE]:
@@ -56,10 +63,24 @@ while not finished:
     screen.blit(backgroundImage, (0,0))
     screen.blit(treasureImage, (treasureX, treasureY))
     screen.blit(playerImage, (playerX,playerY))
-    screen.blit(enemyImage, (enemyX, enemyY))
 
-    if check_collision((playerX, playerY, 35, 40), (treasureX, treasureY, 35, 40)):
+    playerPos = (playerX, playerY, 35, 40)
+    treasurePos = (treasureX, treasureY, 35, 40)
+
+    enemyIndex = 0
+    for enemyX, enemyY, enemyMovingRight in enemies:
+        screen.blit(enemyImage, (enemyX, enemyY))
+        enemyPos = (enemyX, enemyY, 35, 40)
+        if check_collision(playerPos, enemyPos):
+            playerY = 650
+            name = enemyNames[enemyIndex]
+            textLost = font.render("You were killed by" + name, True, red)
+            screen.blit(textLost, (450 - (textLost.get_width() / 2),300 - (textLost.get_height() / 2)))
+        enemyIndex += 1
+
+    if check_collision(playerPos, treasurePos):
         level += 1
+        enemies.append((enemyX - 50 * level, enemyY - 50*level, False))
         textWin = font.render("You've reached level " + str(level), True, black)
         screen.blit(textWin, (450 - (textWin.get_width() / 2),300 - (textWin.get_height() / 2)))
         playerY = 650
